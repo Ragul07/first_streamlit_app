59ff6c1b264431c64155386c53e84fcf854390f5
Ragul07 <36784924+Ragul07@users.noreply.github.com>
2023-09-10 18:54:30 +0530

    Update streamlit_app.py
    added for the viewing the filter condition

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # Let's put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect ("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-# Display the table on the page.
-streamlit.dataframe(my_fruit_list)
+fruits selected = streamlit.multiselect("Pick some fruits:", list (my fruit list.index), ['Avocado', 'Strawberries'])
+fruits to show = my fruit list.loc[fruits selectedl
+#display the table on the page
+streamlit.dataframe(fruits to show)
